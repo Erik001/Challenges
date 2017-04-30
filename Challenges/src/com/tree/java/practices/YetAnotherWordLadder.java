4b492b81bc36949c6284994bff9d7ca1fc13d1c7
torrer02 <torrer02@MXL23Z0S72.enterprisenet.org>
2017-04-30 02:22:19 -0500

    Solved 9 / 11

--- Challenges/src/com/tree/java/practices/YetAnotherWordLadder.java
+++ Challenges/src/com/tree/java/practices/YetAnotherWordLadder.java
@@ -8,23 +8,30 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Stack;
 
 public class YetAnotherWordLadder {
 	public static boolean found;
-	public static boolean hasPassedStartingPoint;
-	public static Map<List<String>, Boolean> transformationPaths = new HashMap<>();
-	private static boolean isSamelevel;
 
 	public static void main(String[] args) {
-		String beginWord = "hit";
-		String endWord = "cog";
-		String wordList[] = { "hot", "dot", "dog", "lot", "log", "cog" };
+		String beginWord = "kiss";
+		String endWord = "tusk";
+		String wordList[] = { "miss", 
+				 "dusk", 
+				 "kiss", 
+				 "musk", 
+				 "tusk", 
+				 "diss", 
+				 "disk", 
+				 "sang", 
+				 "ties", 
+				 "muss" };
 		// int shortestTrans = 0;
-		System.out.println(wordLadder(beginWord, endWord, wordList));
+		System.out.println(new YetAnotherWordLadder().wordLadder(beginWord, endWord, wordList));
 
 	}
 
-	public static int wordLadder(String beginWord, String endWord, String[] wordList) {
+	public int wordLadder(String beginWord, String endWord, String[] wordList) {
 		int shortestTrans = 0;
 		List<String> wordsArrayList = new ArrayList<>(Arrays.asList(wordList));
 		if (beginWord.length() != endWord.length() || !wordsArrayList.contains(endWord)) {
@@ -32,7 +39,7 @@ public class YetAnotherWordLadder {
 		}
 
 		Queue<List<String>> transList = new LinkedList<>();
-		List<List<List<String>>> levels = new LinkedList<>();
+		Stack<List<List<String>>> levels = new Stack();
 		Map<Integer, List<String>> mapOfDiffs = null;
 		transList.add(new LinkedList<>(Arrays.asList(beginWord)));
 		int levelId = 0;
@@ -40,6 +47,8 @@ public class YetAnotherWordLadder {
 			List<List<String>> level = new ArrayList<>(transList.size());
 			levels.add(level);
 			++levelId;
+			List<List<String>> headEleemnt = levels.lastElement();
+			List<List<String>> toAdd = null;
 			for (List<String> strList : transList) {
 				level.add(strList);
 				System.out.printf("Level %d: ", levelId);
@@ -64,25 +73,27 @@ public class YetAnotherWordLadder {
 					} else {
 						break;
 					}
-				}
-				transList.addAll(singleLevelList);
+				}				
+				toAdd = singleLevelList;
 				wordsArrayList = getRemainingStrings(wordsArrayList, transList.peek());
 				transList.poll();
 
 			}
+			transList.addAll(toAdd);
+			List<String> toRemove = new ArrayList<>();
 
 		}
 		return shortestTrans;
 
 	}
 
-	private static List<String> getRemainingStrings(List<String> wordList, List<String> list) {
+	private List<String> getRemainingStrings(List<String> wordList, List<String> list) {
 		wordList.removeAll(list);
 		Collections.sort(wordList);
 		return wordList;
 	}
 
-	private static Map<Integer, List<String>> getDiffsMap(String beginWord, List<String> wordList) {
+	private Map<Integer, List<String>> getDiffsMap(String beginWord, List<String> wordList) {
 		List listOfStrings = null;
 		Map<Integer, List<String>> mapOfDiffs = new HashMap<>();
 		for (String temWord : wordList) {
@@ -98,7 +109,7 @@ public class YetAnotherWordLadder {
 		return mapOfDiffs;
 	}
 
-	public static int numDiffBtwnStrings(String s1, String s2) {
+	public int numDiffBtwnStrings(String s1, String s2) {
 		int result = s1.length();
 		char[] s1Array = s1.toCharArray();
 		char[] s2Array = s2.toCharArray();
